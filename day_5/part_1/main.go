f411105054d1e480869dee1636e38c78ec3e101b
James Metz <jamesmetz1231@gmail.com>
2025-12-15 22:41:53 -0500

    finishing day 5 part 1

--- day_5/part_1/main.go
+++ day_5/part_1/main.go
@@ -2,16 +2,56 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
 	"path/filepath"
+	"slices"
+	"strconv"
 	"strings"
 )
+
+// int64 needed for these values
+// (which int should work for on fedora, but use it explicitly for cross platform)
+type idSet struct {
+	max int64
+	min int64
+}
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
+
+func quickSort(list []int64) []int64 {
+	if len(list) <= 1 {
+		return list
+	}
+
+	pivot := rand.Intn(len(list))
+	pv := list[pivot]
+
+	left := []int64{}
+	right := []int64{}
+
+	for i, v := range list {
+		if i == pivot {
+			continue
+		}
+		if v < pv {
+			left = append(left, v)
+		} else {
+			right = append(right, v)
+		}
+	}
+
+	left = quickSort(left)
+	right = quickSort(right)
+
+	return slices.Concat(left, []int64{pv}, right)
+}
+
 func main() {
 	wdir, err := os.Getwd()
 	check(err)
@@ -22,5 +62,58 @@ func main() {
 
 	contents := string(data)
 	lines := strings.Split(contents, "\n")
-	fmt.Println("Hello World", lines)
+	fmt.Println("Hello World", len(lines))
+
+	idRanges := []idSet{}
+	idsToCheck := []int64{}
+
+	for i, l := range lines {
+		if len(l) == 0 {
+			continue
+		}
+
+		if strings.Contains(l, "-") {
+			ids := strings.Split(l, "-")
+			if len(ids) != 2 {
+				panic(fmt.Errorf("expected line %d to have two ids separated by '-'", i))
+			}
+			minId, err := strconv.ParseInt(ids[0], 10, 64)
+			check(err)
+
+			maxId, err := strconv.ParseInt(ids[1], 10, 64)
+			check(err)
+
+			idRanges = append(idRanges, idSet{
+				min: minId,
+				max: maxId,
+			})
+		} else {
+			id, err := strconv.ParseInt(l, 10, 64)
+			check(err)
+			idsToCheck = append(idsToCheck, id)
+		}
+	}
+	sortedIds := quickSort(idsToCheck)
+
+	freshIds := []int64{}
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+	log.Println("Starting the double nested for loop.")
+	for _, idRange := range idRanges {
+		for _, id := range sortedIds {
+			if slices.Contains(freshIds, id) {
+				continue // skip if item status known
+			}
+			if id >= idRange.min && id <= idRange.max {
+				freshIds = append(freshIds, id)
+			} else {
+				if id > idRange.max {
+					break // range has no more ids worth checking bc asc sort
+					// i checked this, and this if statement made it run in
+					// .028 sec vs .042 sec
+				}
+			}
+		}
+	}
+	log.Println("Finished the double nested for loop.")
+	fmt.Printf("Found %d Fresh Items\n", len(freshIds))
 }
